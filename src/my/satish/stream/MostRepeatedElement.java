ca99c5312f3f2a1871613e74f267b6f3c0cc75dc
Satish522 <satishsubudhi522@gmail.com>
2023-11-26 09:59:09 +0530

    stream most repeated program

--- src/my/satish/stream/MostRepeatedElement.java
+++ src/my/satish/stream/MostRepeatedElement.java
@@ -1,5 +1,25 @@
 package my.satish.stream;
 
-public class MostRepeatedElement {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+public class MostRepeatedElement {
+	
+	public static void main(String arg[]) {
+		int arr[]= {1,1,2,3,2,4,3,2,1,1,3,4,2,1,2,2,2,2,2,2};
+		
+		List<Integer> number = Arrays.stream(arr).boxed()
+				.collect(Collectors.groupingBy(i-> i , Collectors.counting()))
+				.entrySet().stream()
+				.sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+				.map(Map.Entry::getKey)
+				.limit(2)
+				.collect(Collectors.toList());
+		
+		System.out.println(number);
+		
+	}
 }
